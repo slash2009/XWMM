4901d30efdd16e7544b81e4c007187531518e4f7
Andrew Fyfe <andrew@neptune-one.net>
2014-03-01 15:18:47 +0000

    Revert "Delete the old movies folder and rename movies2 to movies [aa39136]"
    Looking back this work isn't anywhere near ready for inclusion. I never finished
    implementing several of the features.
    
    This reverts commit ec9b70f6e5d7c75562fa25d51a3a296fe53eb7b1.

---
+++ movies/movieset.js
@@ -0,0 +1,153 @@
+//------------ Movie All Sets (including orphans) ----------------
+
+var MovieSetRecord = Ext.data.Record.create([
+   {name: 'idSet', mapping: 'setid', type: 'int'},
+   {name: 'strSet', mapping: 'title'}
+]);
+
+var MovieSetStore = new Ext.data.GroupingStore({
+    sortInfo: {field: 'strSet', direction: "ASC"},
+    //autoLoad: true,
+    proxy: new Ext.data.XBMCProxy({
+        url: "/jsonrpc",
+        xbmcParams :{"jsonrpc": "2.0", "method": "VideoLibrary.GetMovieSets", "params": {"properties": ["title"]},"id": 1}
+    }),
+    reader: new Ext.data.JsonReader({
+        root: 'result.sets'
+    }, MovieSetRecord)
+});
+
+var MoviesInSetcolModel = new Ext.grid.ColumnModel([
+        {header: "#", dataIndex: 'idMovie', hidden: true},
+        {header: "Movie Title", dataIndex: 'movieinset', width: 200}
+    ]);
+
+
+var MoviesInSetRecord = Ext.data.Record.create([
+   {name: 'idMovie', mapping: 'movieid'},
+   {name: 'movieinset', mapping: 'title'}
+]);
+
+
+var MoviesInSetStore = new Ext.data.GroupingStore({
+    sortInfo: {field: 'movieinset', direction: "ASC"},
+    id: 'moviesinsetstore',
+    proxy: new Ext.data.XBMCProxy({
+        url: "/jsonrpc",
+        xbmcParams :{"jsonrpc": "2.0", "method": "VideoLibrary.GetMovieSetDetails", "params": {"setid": 2147483647, "movies": {"properties": ["title"]} }, "id": 1} //dummy ID will be replaced by selected setid when set selected
+    }),
+    reader: new Ext.data.JsonReader({
+        root:'result.setdetails.movies'
+       }, MoviesInSetRecord)
+});
+
+
+var MovieInSetGrid = new Ext.grid.GridPanel({
+    width:230,
+    height: 290,
+    cm: MoviesInSetcolModel,
+    title: 'Movies in Set',
+    enableDragDrop: false,
+    disableSelection : true,
+    store: MoviesInSetStore,
+    viewConfig: {forceFit: true}
+});
+
+// ----- Movie Set Management Window
+
+function onAddMovieSet(btn, ev) {
+        var u = new MovieSetMgmtGrid.store.recordType({
+                strSet: 'New Set',
+        idSet: '-1' // flag as new record
+        });
+        editor.stopEditing();
+        MovieSetStore.insert(0, u);
+        editor.startEditing(0)
+}
+
+function onDeleteMovieSet() {
+    var rec = MovieSetMgmtGrid.getSelectionModel().getSelected();
+    if (!rec) {return false;}
+
+
+    MovieSetStore.remove(rec)
+    MoviesInSetStore.each( function (movieRecord)
+        {
+            xbmcJsonRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.SetMovieDetails", "params": {"movieid": '+ movieRecord.data.idMovie +', "set": ""}, "id": 1}');
+        }, this);
+    MovieSetStore.reload()
+}
+
+var MovieSetEditor = new Ext.ux.grid.RowEditor({
+    saveText: 'Update',
+    listeners: {
+        afteredit: function(roweditor, changes, record, rowIndex) {
+            if (record.data.idSet == -1) {
+                //should add a bubble to tell user to add movies in the set
+            }
+            else {
+                MoviesInSetStore.each( function (movieRecord)
+                    {
+                        xbmcJsonRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.SetMovieDetails", "params": {"movieid": '+ movieRecord.data.idMovie +', "set": "'+ changes.strSet +'"}, "id": 1}');
+                    }, this);
+                MovieSetStore.reload()
+            }
+        }
+    }
+});
+
+var MovieSetMgmtGrid = new Ext.grid.GridPanel({
+    id: 'moviesetmgmtgrid',
+    width: 250,
+    height: 290,
+    columns: [
+        {header: "#", dataIndex: 'idSet', hidden: true},
+        {header: "Set Name", width: 200, editor: new Ext.form.TextField({allowBlank: false}),dataIndex: 'strSet'}
+    ],
+    clicksToEdit: 1,
+    title: 'Sets',
+    enableDragDrop: false,
+    stripeRows: true,
+    selModel: new Ext.grid.RowSelectionModel({
+        singleSelect: true,
+        listeners : {
+        rowselect: function(sm, row, rec) {
+                MoviesInSetStore.proxy.conn.xbmcParams = {"jsonrpc": "2.0", "method": "VideoLibrary.GetMovieSetDetails", "params": {"setid": rec.data.idSet, "movies": {"properties": ["title"]} }, "id": 1};
+                MoviesInSetStore.load();
+            }
+        }
+    }),
+    plugins: [MovieSetEditor],
+    store: MovieSetStore,
+    tbar: [{
+        text: 'Add',
+        //disabled: 'true', //disabled as no method of adding new sets via JSON currently
+        iconCls: 'silk-add',
+        handler: onAddMovieSet
+    }, '-', {
+        text: 'Delete',
+        //disabled: 'true', //disabled as no method of deleting sets via JSON currently
+        iconCls: 'silk-delete',
+        handler: onDeleteMovieSet
+    }, '-'],
+    viewConfig: {forceFit: true}
+});
+
+var winMovieSet = new Ext.Window({
+    layout:'table',
+    layoutConfig: {columns:2},
+    title: 'Movie Set Management',
+    width:500,
+    height:300,
+    closeAction:'hide',
+    plain: true,
+    items: [
+        MovieSetMgmtGrid, MovieInSetGrid
+    ],
+    buttons: [{
+        text: 'Close',
+        handler: function(){
+            winMovieSet.hide()
+        }
+    }]
+});
---
+++ movies/startapp.js
@@ -0,0 +1,186 @@
+
+// -----------------------------------------
+// startapp.js
+//------------------------------------------
+
+Ext.onReady(function() {
+
+    menuBar.add({
+            xtype: 'tbspacer'
+        },{
+            xtype: 'tbbutton',
+            text: 'Tools',
+            width: 60,
+            menu: [{
+                text: 'Manage Genres',
+                iconCls: 'silk-plugin',
+                disabled: 'true',
+                handler: function(){winGenre.show()}
+            },{
+                text: 'Manage Actors',
+                iconCls: 'silk-plugin',
+                disabled: 'true',
+                handler: function(){window.location = '../actors/index.html'}
+            },{
+                text: 'Manage Movie Sets',
+                iconCls: 'silk-plugin',
+                handler: function(){winMovieSet.show()}
+            },{
+                text: 'Toggle articles in title sort',
+                iconCls: 'silk-plugin',
+                handler: function(){
+                    // extjs cookies aren't available until v3.4.0, so we'll use cookies.js
+                    var sortArticles = docCookies.getItem('sortArticles');
+                    if (sortArticles == '1') {
+                        docCookies.removeItem('sortArticles');
+                    } else {
+                        docCookies.setItem('sortArticles', '1', 'Infinity');
+                    }
+                    window.location.reload();
+                }
+            },{
+                text: 'Export to HTML',
+                menu: [{
+                    text: 'All Movies',
+                    iconCls: 'silk-grid',
+                    handler: function(){moviesHTML()}
+                },{
+                    text: 'Watched Movies',
+                    iconCls: 'silk-grid',
+                    handler: function(){watchedMoviesHTML()}
+                },{
+                    text: 'Unwatched Movies',
+                    iconCls: 'silk-grid',
+                    handler: function(){unwatchedMoviesHTML()}
+
+                }]
+            }]
+        },{
+            text: 'Quicksearch:',
+            tooltip: 'Quickly search through the grid.'
+        },{
+            xtype: 'text',
+            tag: 'input',
+            id: 'quicksearch',
+            size: 30,
+            value: '',
+            style: 'background: #F0F0F9;'
+    });
+
+    menuBar.add({
+        text: 'X',
+        tooltip: 'Clear quicksearch',
+        handler: function() {
+            var item = Ext.getCmp('searchBox');
+            if (item.getValue().length!=0) {
+                item.setValue('');
+                storeMovie.clearFilter();
+            }
+        }
+    });
+
+    menuBar.add({
+            xtype: 'tbfill'
+        },{
+            text: myVersion
+    });
+
+
+    MovieSetStore.load();
+
+    startMyApp()
+
+    function startMyApp() {
+
+
+        var App = new Movie.Mainpanel({
+            renderTo: Ext.getBody()
+        });
+
+        storegenre.proxy.conn.xbmcParams = {"jsonrpc": "2.0", "method": "VideoLibrary.GetGenres", "params": {"type": "movie"},"id": 1};
+        storeMovie.load();
+        storegenre.load();
+
+        Ext.QuickTips.init();
+
+        // begin search config
+
+        var searchStore = new Ext.data.SimpleStore({
+            fields: ['query'],
+        data: []
+        });
+
+        var searchBox = new Ext.form.ComboBox({
+            id: 'searchBox',
+            store: searchStore,
+            displayField: 'query',
+            typeAhead: false,
+            mode: 'local',
+            triggerAction: 'all',
+            applyTo: 'quicksearch',
+            hideTrigger: true
+        });
+
+        var searchRec = Ext.data.Record.create([
+            {name: 'query', type: 'string'}
+        ]);
+
+        var onFilteringBeforeQuery = function(e) {
+        //grid.getSelectionModel().clearSelections();
+            if (this.getValue().length==0) {
+                        storeMovie.clearFilter();
+                    } else {
+                        var value = this.getValue().replace(/^\s+|\s+$/g, "");
+                        if (value=="")
+                            return;
+                        storeMovie.filter('Movietitle', value, true, false);
+                    }
+        };
+
+        var onQuickSearchBeforeQuery = function(e) {
+            if (this.getValue().length==0) {
+            } else {
+                var value = this.getValue().replace(/^\s+|\s+$/g, "");
+                if (value=="")
+                    return;
+                searchStore.clearFilter();
+                var vr_insert = true;
+                searchStore.each(function(r) {
+                    if (r.data['query'].indexOf(value)==0) {
+                        // backspace
+                        vr_insert = false;
+                        return false;
+                    } else if (value.indexOf(r.data['query'])==0) {
+                        // forward typing
+                        searchStore.remove(r)
+                    }
+                });
+                if (vr_insert==true) {
+                    searchStore.each(function(r) {
+                        if (r.data['query']==value) {
+                            vr_insert = false
+                        }
+                    });
+                }
+                if (vr_insert==true) {
+                    var vr = new searchRec({query: value});
+                    searchStore.insert(0, vr)
+                }
+                var ss_max = 4; // max 5 query history, starts counting from 0; 0==1,1==2,2==3,etc
+                if (searchStore.getCount()>ss_max) {
+                    var ssc = searchStore.getCount();
+                    var overflow = searchStore.getRange(ssc-(ssc-ss_max), ssc);
+                    for (var i=0; i<overflow.length; i++) {
+                        searchStore.remove(overflow[i])
+                    }
+                }
+        }
+        };
+
+        searchBox.on("beforequery", onQuickSearchBeforeQuery);
+        searchBox.on("beforequery", onFilteringBeforeQuery);
+        searchBox.on("select", onFilteringBeforeQuery)
+        // end search
+    }
+
+});
