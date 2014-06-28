slash2009/XWMM

commit 683684c774f874dc2c4f6ba6242ad358d4608f9d

Clean up whitespace

tvshows/include.js
@@ -1,7 +1,6 @@
-
 // -----------------------------------------
 // TV SHOW include.js
-//------------------------------------------ 
+//------------------------------------------
 
 Ext.BLANK_IMAGE_URL = '../extjs/resources/images/default/s.gif';
 var responseFinale = [];
@@ -14,65 +13,65 @@ var detailPanel;
 
 
 var gridContextMenu = new Ext.menu.Menu({
-	items: [
-		{ text: 'Mark as watched', handler: setWatched },
-		{ text: 'Mark as unwatched', handler: setUnwatched }		
-	]
+    items: [
+        { text: 'Mark as watched', handler: setWatched },
+        { text: 'Mark as unwatched', handler: setUnwatched }
+    ]
 });
 
 function setWatched() {
-	if (selectedEpisode.data.watched == "") {
-		setXBMCwatched(selectedEpisode.data.idFile);
-		selectedEpisode.data.watched ="1";
-		EpisodeGrid.getView().refresh();
-	}
+    if (selectedEpisode.data.watched == "") {
+        setXBMCwatched(selectedEpisode.data.idFile);
+        selectedEpisode.data.watched ="1";
+        EpisodeGrid.getView().refresh();
+    }
 }
 
 function setUnwatched() {
-	if (selectedEpisode.data.watched != "") {
-		setXBMCunwatched(selectedEpisode.data.idFile);
-		selectedEpisode.data.watched = "";
-		EpisodeGrid.getView().refresh()
-	}
+    if (selectedEpisode.data.watched != "") {
+        setXBMCunwatched(selectedEpisode.data.idFile);
+        selectedEpisode.data.watched = "";
+        EpisodeGrid.getView().refresh()
+    }
 }
 
 function updateXBMCAll() {
-	Ext.MessageBox.show({
-		title: 'Please wait',
-		msg: 'Saving changes',
-		progressText: 'Checking changes...',
-		width:300,
-		progress:true,
-		closable:false,
-		animEl: 'samplebutton'
-	});
-	var f = function(v){
+    Ext.MessageBox.show({
+        title: 'Please wait',
+        msg: 'Saving changes',
+        progressText: 'Checking changes...',
+        width:300,
+        progress:true,
+        closable:false,
+        animEl: 'samplebutton'
+    });
+    var f = function(v){
         return function(){
-		if(v == 30){
+        if(v == 30){
             Ext.MessageBox.hide();
         }else{
             var i = v/29;
-			if (v == 1) {
-				myText = 'Checking changes...';
-				if (EpisodedetailPanel.getForm().isDirty()) {
-					updateXBMCTables(EpisodedetailPanel.getForm(), 'episode', EpisodeGrid.getSelectionModel().getSelected().data.episodeid);
-					myText = 'updating Episode info';
-				}
-			}
+            if (v == 1) {
+                myText = 'Checking changes...';
+                if (EpisodedetailPanel.getForm().isDirty()) {
+                    updateXBMCTables(EpisodedetailPanel.getForm(), 'episode', 'episode', EpisodeGrid.getSelectionModel().getSelected().data.episodeid);
+                    myText = 'updating Episode info';
+                }
+            }
                if (v == 10) {
-				if (TVShowdetailPanel.getForm().isDirty()) {
-					updateXBMCTables(TVShowdetailPanel.getForm(), 'tvshow', TvShowGrid.getSelectionModel().getSelected().data.tvshowid);
-					myText = 'updating TV Show info';
-						//need commit here
-				}
-			}
-			if (v == 20) {
-				if (Ext.getCmp('genreString').isDirty()) {
-					updateXBMCGenreTvshow();		
-					myText = 'updating Genres'
-				}
-			}
-			Ext.MessageBox.updateProgress(i, myText);
+                if (TVShowdetailPanel.getForm().isDirty()) {
+                    updateXBMCTables(TVShowdetailPanel.getForm(), 'tvshow', 'tvshow', TvShowGrid.getSelectionModel().getSelected().data.tvshowid);
+                    myText = 'updating TV Show info';
+                        //need commit here
+                }
+            }
+            if (v == 20) {
+                if (Ext.getCmp('genreString').isDirty()) {
+                    updateXBMCGenreTvshow();
+                    myText = 'updating Genres'
+                }
+            }
+            Ext.MessageBox.updateProgress(i, myText);
         }
         }
     };
@@ -83,87 +82,87 @@ function updateXBMCAll() {
 
 function movieGenreChange(sm){
 
-	var sel = sm.getSelections();
-	var strTemp = "";
-	for (var i = 0; i < sel.length; i++) {
-		if (strTemp == ""){strTemp = sel[i].data.label}
-			else{ strTemp = strTemp+' / '+sel[i].data.label};
-	}
-	selectedTvShow.data.genre = strTemp;
-	Ext.getCmp('genreString').setValue(strTemp)
+    var sel = sm.getSelections();
+    var strTemp = "";
+    for (var i = 0; i < sel.length; i++) {
+        if (strTemp == ""){strTemp = sel[i].data.label}
+            else{ strTemp = strTemp+' / '+sel[i].data.label};
+    }
+    selectedTvShow.data.genre = strTemp;
+    Ext.getCmp('genreString').setValue(strTemp)
 }
 
 function updateTvShowForms(r) {
 
-	tvshowStars.updateSrc(r);
-	Ext.getCmp('tvshowcover').updateSrc(r.data.banner)
-	//Ext.getCmp('seasoncover').updateSrc(r, -1);
-	var myForm = Ext.getCmp('tvShowdetailPanel');
-	myForm.getForm().loadRecord(r)
+    tvshowStars.updateSrc(r);
+    Ext.getCmp('tvshowcover').updateSrc(r.data.banner)
+    //Ext.getCmp('seasoncover').updateSrc(r, -1);
+    var myForm = Ext.getCmp('tvShowdetailPanel');
+    myForm.getForm().loadRecord(r)
 }
 
 function updateEpisodeForms(r) {
 
-	episodeStars.updateSrc(r);
-	//Ext.getCmp('seasoncover').updateSrc(r, r.data.EpisodeSeason);
-	
-	Ext.getCmp('episodedetailPanel').getForm().loadRecord(r);
-	
-	if (r.data.streamdetails != null) {
-		Ext.getCmp('videocodec').getEl().dom.src = "../images/flags/"+r.data.streamdetails.video[0].codec+".png";
-		Ext.getCmp('aspect').getEl().dom.src = "../images/flags/"+findAspect(r.data.streamdetails.video[0].aspect)+".png";
-		Ext.getCmp('resolution').getEl().dom.src = "../images/flags/"+findResolution(r.data.streamdetails.video[0].width)+".png";			
-		Ext.getCmp('audiochannels').getEl().dom.src = "../images/flags/"+r.data.streamdetails.audio[0].channels+"c.png";
-		Ext.getCmp('audiocodec').getEl().dom.src = "../images/flags/"+r.data.streamdetails.audio[0].codec+".png";
-	}
-	Ext.getCmp('filedetailPanel').getForm().loadRecord(r);
+    episodeStars.updateSrc(r);
+    //Ext.getCmp('seasoncover').updateSrc(r, r.data.EpisodeSeason);
+
+    Ext.getCmp('episodedetailPanel').getForm().loadRecord(r);
+
+    if (r.data.streamdetails != null) {
+        Ext.getCmp('videocodec').getEl().dom.src = "../images/flags/"+r.data.streamdetails.video[0].codec+".png";
+        Ext.getCmp('aspect').getEl().dom.src = "../images/flags/"+findAspect(r.data.streamdetails.video[0].aspect)+".png";
+        Ext.getCmp('resolution').getEl().dom.src = "../images/flags/"+findResolution(r.data.streamdetails.video[0].width)+".png";
+        Ext.getCmp('audiochannels').getEl().dom.src = "../images/flags/"+r.data.streamdetails.audio[0].channels+"c.png";
+        Ext.getCmp('audiocodec').getEl().dom.src = "../images/flags/"+r.data.streamdetails.audio[0].codec+".png";
+    }
+    Ext.getCmp('filedetailPanel').getForm().loadRecord(r);
 }
 
 function GettvShowDetails(r){
 
-	var jsonResponse = xbmcJsonRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetTvShowDetails", "params": {"tvshowid": '+r.data.tvshowid+', "properties": ["title", "genre", "year", "rating",  "plot", "playcount", "studio", "mpaa",  "premiered", "votes", "fanart", "thumbnail", "file", "episodeguide" ]}, "id": 1}');
+    var jsonResponse = xbmcJsonRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.GetTvShowDetails", "params": {"tvshowid": '+r.data.tvshowid+', "properties": ["title", "genre", "year", "rating",  "plot", "playcount", "studio", "mpaa",  "premiered", "votes", "fanart", "thumbnail", "file", "episodeguide" ]}, "id": 1}');
 
-	mergeJson(r.data, jsonResponse.tvshowdetails);
-	
-	//fix up some data retrieved
-	r.data.fanart = jsonResponse.tvshowdetails.fanart.replace(/image:\/\//g, "").slice(0,-1);
-	r.data.thumbnail = jsonResponse.tvshowdetails.thumbnail.replace(/image:\/\//g, "").slice(0,-1);
-	updateTvShowForms(r);
-	r.data.details = true
+    mergeJson(r.data, jsonResponse.tvshowdetails);
+
+    //fix up some data retrieved
+    r.data.fanart = jsonResponse.tvshowdetails.fanart.replace(/image:\/\//g, "").slice(0,-1);
+    r.data.thumbnail = jsonResponse.tvshowdetails.thumbnail.replace(/image:\/\//g, "").slice(0,-1);
+    updateTvShowForms(r);
+    r.data.details = true
 }
 
 function updateXBMCGenreTvshow(){
-	var parmArray = [];
-	var jsParam = '';
-	var modifiedGenre = Genregrid.getSelectionModel().getSelections();
-	
-	//update current.selected genres
-	var currentTVShow = Ext.getCmp('tvshowgrid').getSelectionModel().getSelected();
-	
-	currentTVShow.data.selectedGenre = modifiedGenre;
-	tvshowid = currentTVShow.data.tvshowid;
+    var parmArray = [];
+    var jsParam = '';
+    var modifiedGenre = Genregrid.getSelectionModel().getSelections();
 
-	for (var i = 0; i < modifiedGenre.length; i++){
-		parmArray[i] = modifiedGenre[i].data.label;
-	}
-	if (parmArray.length == 1) {
-		jsParam = '"' + parmArray[0] + '"';
-		}
-	else {
-		jsParam = '"' + parmArray.join('","') + '"';
-		}
-	xbmcJsonRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.SetTVShowDetails", "params": {"tvshowid": '+tvshowid+', "genre": ['+jsParam+']}, "id": 1}');
+    //update current.selected genres
+    var currentTVShow = Ext.getCmp('tvshowgrid').getSelectionModel().getSelected();
+
+    currentTVShow.data.selectedGenre = modifiedGenre;
+    tvshowid = currentTVShow.data.tvshowid;
+
+    for (var i = 0; i < modifiedGenre.length; i++){
+        parmArray[i] = modifiedGenre[i].data.label;
+    }
+    if (parmArray.length == 1) {
+        jsParam = '"' + parmArray[0] + '"';
+        }
+    else {
+        jsParam = '"' + parmArray.join('","') + '"';
+        }
+    xbmcJsonRPC('{"jsonrpc": "2.0", "method": "VideoLibrary.SetTVShowDetails", "params": {"tvshowid": '+tvshowid+', "genre": ['+jsParam+']}, "id": 1}');
 }
 
 // Query XBMC DB genrelinktvshow
 function GetTvshowGenres(record){
-	var responseArr = [];
-	var myGenres = record.data.TVGenre.split('/');
-	
-	for (var i = 0; i < myGenres.length; i++) {
-		responseArr[i]= storegenre.findExact('label',removeSpace(myGenres[i]),0,false,false)
-	}
-	updateGenreGrid(responseArr)
+    var responseArr = [];
+    var myGenres = record.data.TVGenre.split('/');
+
+    for (var i = 0; i < myGenres.length; i++) {
+        responseArr[i]= storegenre.findExact('label',removeSpace(myGenres[i]),0,false,false)
+    }
+    updateGenreGrid(responseArr)
 }
 
 
@@ -186,19 +185,19 @@ function checkWatechedInt(val) {
 }
 
 var episodecolModel = new Ext.grid.ColumnModel([
-	{header: "#", dataIndex: 'episode', width: 30},
-	{header: "title", dataIndex: 'title', width: 130},
-	{header: "W", dataIndex: 'playcount', width: 25, renderer: checkWatched}
+    {header: "#", dataIndex: 'episode', width: 30},
+    {header: "title", dataIndex: 'title', width: 130},
+    {header: "W", dataIndex: 'playcount', width: 25, renderer: checkWatched}
 ]);
-	
+
 var tvShowcolModel = new Ext.grid.ColumnModel([
-	{header: "Title", width: 155, dataIndex: 'title'},
-	{header: "W", dataIndex: 'playcount', width: 25, renderer: checkWatched}
+    {header: "Title", width: 155, dataIndex: 'title'},
+    {header: "W", dataIndex: 'playcount', width: 25, renderer: checkWatched}
 ]);
 
 var seasoncolModel = new Ext.grid.ColumnModel([
-		{header: "#", dataIndex: 'season', hidden: true},
-		{header: "Season", width: 115, dataIndex: 'label'}
+        {header: "#", dataIndex: 'season', hidden: true},
+        {header: "Season", width: 115, dataIndex: 'label'}
     ]);
-	
+
 //Ext.BLANK_IMAGE_URL = 'extjs/resources/../images/stars/default/s.gif';

actors/include.js
@@ -1,14 +1,13 @@
-
 // -----------------------------------------
 // Actors include.js
-//------------------------------------------ 
+//------------------------------------------
 
 // ------------ Actor in Movies information -------------
 
 var ActorMoviecolModel = new Ext.grid.ColumnModel([
-		{header: "#", dataIndex: 'idMovie', hidden: true},
-		{header: "Movie Title", dataIndex: 'c00', width: 200},
-		{header: "Role", dataIndex: 'strRole', width: 200}
+        {header: "#", dataIndex: 'idMovie', hidden: true},
+        {header: "Movie Title", dataIndex: 'c00', width: 200},
+        {header: "Role", dataIndex: 'strRole', width: 200}
     ]);
 
 var ActorMovieRecord = Ext.data.Record.create([
@@ -19,43 +18,43 @@ var ActorMovieRecord = Ext.data.Record.create([
 ]);
 
 var ActorMovieStore = new Ext.data.Store({
-	storeId: 'actormoviestore',
-	sortInfo: {field: 'idMovie', direction: "ASC"},
-	reader: new Ext.data.JsonXBMCReader({
-		root:'data'	       
+    storeId: 'actormoviestore',
+    sortInfo: {field: 'idMovie', direction: "ASC"},
+    reader: new Ext.data.JsonXBMCReader({
+        root:'data'
        }, ActorMovieRecord),
-	autoLoad: false,
-	url: '/' 
+    autoLoad: false,
+    url: '/'
 });
 
 ActorMovieGrid = new Ext.grid.GridPanel({
-	cm: ActorMoviecolModel,
-	id: 'actormovieGrid',
-	title: 'Role in Movies',
-	enableDragDrop: false,
-	stripeRows: true,
-	viewconfig: {forceFit: true},
-	selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
-	split: true,
-	height: 310,
-	tbar: [{
-		text: 'Add',
-		iconCls: 'silk-add',
-		handler: onAddActorMovie
-	}, '-', {
-		text: 'Delete',
-		iconCls: 'silk-delete',
-		handler: onDeleteActorMovie
-	}, '-'],
-	store: ActorMovieStore
-}); 
+    cm: ActorMoviecolModel,
+    id: 'actormovieGrid',
+    title: 'Role in Movies',
+    enableDragDrop: false,
+    stripeRows: true,
+    viewconfig: {forceFit: true},
+    selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
+    split: true,
+    height: 310,
+    tbar: [{
+        text: 'Add',
+        iconCls: 'silk-add',
+        handler: onAddActorMovie
+    }, '-', {
+        text: 'Delete',
+        iconCls: 'silk-delete',
+        handler: onDeleteActorMovie
+    }, '-'],
+    store: ActorMovieStore
+});
 
 // ------------ Actor in TV Show information -------------
 
 var ActorTvshowcolModel = new Ext.grid.ColumnModel([
-		{header: "#", dataIndex: 'idShow', hidden: true},
-		{header: "TV Show Title", dataIndex: 'c00', width: 200},
-		{header: "Role", dataIndex: 'strRole', width: 200}
+        {header: "#", dataIndex: 'idShow', hidden: true},
+        {header: "TV Show Title", dataIndex: 'c00', width: 200},
+        {header: "Role", dataIndex: 'strRole', width: 200}
     ]);
 
 var ActorTvshowRecord = Ext.data.Record.create([
@@ -66,44 +65,44 @@ var ActorTvshowRecord = Ext.data.Record.create([
 ]);
 
 var ActorTvshowStore = new Ext.data.Store({
-	storeId: 'actortvshowstore',
-	sortInfo: {field: 'idShow', direction: "ASC"},
-	reader: new Ext.data.JsonXBMCReader({
-		root:'data'	       
+    storeId: 'actortvshowstore',
+    sortInfo: {field: 'idShow', direction: "ASC"},
+    reader: new Ext.data.JsonXBMCReader({
+        root:'data'
        }, ActorTvshowRecord),
-	autoLoad: false,
-	url: '/' 
+    autoLoad: false,
+    url: '/'
 });
 
 ActorTvshowGrid = new Ext.grid.GridPanel({
-	cm: ActorTvshowcolModel,
-	id: 'actortvshowGrid',
-	title: 'Role in TV Shows',
-	enableDragDrop: false,
-	stripeRows: true,
-	viewconfig: {forceFit: true},
-	selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
-	split: true,
-	height: 310,
-	tbar: [{
-		text: 'Add',
-		iconCls: 'silk-add',
-		handler: onAddActorTVShow
-	}, '-', {
-		text: 'Delete',
-		iconCls: 'silk-delete',
-		handler: onDeleteActorTVShow
-	}, '-'],
-	store: ActorTvshowStore
+    cm: ActorTvshowcolModel,
+    id: 'actortvshowGrid',
+    title: 'Role in TV Shows',
+    enableDragDrop: false,
+    stripeRows: true,
+    viewconfig: {forceFit: true},
+    selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
+    split: true,
+    height: 310,
+    tbar: [{
+        text: 'Add',
+        iconCls: 'silk-add',
+        handler: onAddActorTVShow
+    }, '-', {
+        text: 'Delete',
+        iconCls: 'silk-delete',
+        handler: onDeleteActorTVShow
+    }, '-'],
+    store: ActorTvshowStore
 });
 
 // ------------ Actor in Episodes information -------------
 
 var ActorEpisodecolModel = new Ext.grid.ColumnModel([
-		{header: "#", dataIndex: 'idEpisode', hidden: true},
-		{header: "TV Show", dataIndex: 'TVShow', width: 150},
-		{header: "Episode Title", dataIndex: 'c00', width: 200},
-		{header: "Role", dataIndex: 'strRole', width: 120}
+        {header: "#", dataIndex: 'idEpisode', hidden: true},
+        {header: "TV Show", dataIndex: 'TVShow', width: 150},
+        {header: "Episode Title", dataIndex: 'c00', width: 200},
+        {header: "Role", dataIndex: 'strRole', width: 120}
     ]);
 
 var ActorEpisodeRecord = Ext.data.Record.create([
@@ -115,64 +114,64 @@ var ActorEpisodeRecord = Ext.data.Record.create([
 ]);
 
 var ActorEpisodeStore = new Ext.data.Store({
-	storeId: 'actorepisodestore',
-	sortInfo: {field: 'c00', direction: "ASC"},
-	reader: new Ext.data.JsonXBMCReader({
-		root:'data'	       
+    storeId: 'actorepisodestore',
+    sortInfo: {field: 'c00', direction: "ASC"},
+    reader: new Ext.data.JsonXBMCReader({
+        root:'data'
        }, ActorEpisodeRecord),
-	autoLoad: false,
-	url: '/' 
+    autoLoad: false,
+    url: '/'
 });
 
 ActorEpisodeGrid = new Ext.grid.GridPanel({
-	cm: ActorEpisodecolModel,
-	id: 'actorepisodeGrid',
-	title: 'Role in Episodes',
-	enableDragDrop: false,
-	stripeRows: true,
-	viewconfig: {forceFit: true},
-	selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
-	region: 'center',
-	split: true,
-	height: 310,
-	tbar: [{
-		text: 'Add',
-		iconCls: 'silk-add',
-		handler: onAddActorEpisode
-	}, '-', {
-		text: 'Delete',
-		iconCls: 'silk-delete',
-		handler: onDeleteActorEpisode
-	}, '-'],
-	store: ActorEpisodeStore
+    cm: ActorEpisodecolModel,
+    id: 'actorepisodeGrid',
+    title: 'Role in Episodes',
+    enableDragDrop: false,
+    stripeRows: true,
+    viewconfig: {forceFit: true},
+    selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
+    region: 'center',
+    split: true,
+    height: 310,
+    tbar: [{
+        text: 'Add',
+        iconCls: 'silk-add',
+        handler: onAddActorEpisode
+    }, '-', {
+        text: 'Delete',
+        iconCls: 'silk-delete',
+        handler: onDeleteActorEpisode
+    }, '-'],
+    store: ActorEpisodeStore
 });
 
 
 // ------------ Actor information -------------
 
 var ActorPicture = new Ext.Container ({
-	id: 'actorpicture',
-	border: 0,
-	// width: 96,
-	height:300,
-	autoEl: {tag: 'img', src: "../images/DefaultArtist.png"},
-	updateSrc :function(r){
-		if (r.data.strThumb == "")	{
-			this.el.dom.src = "../images/DefaultArtist.png"
-		}
-		else {
-			var thumb =  r.data.strThumb.replace(/<\/thumb>/g, "");
-			thumb = thumb.replace(/<thumb>/g, "");
-			var thumbCrc = FindCRC("actor"+r.data.strActor);
-			//console.log(r.data.strActor,' - ',thumbCrc);
-			this.el.dom.src = "../../vfs/special://masterprofile/Thumbnails/Video/"+thumbCrc.substring(0,1)+"/"+thumbCrc+".tbn";
-		}
-	}
+    id: 'actorpicture',
+    border: 0,
+    // width: 96,
+    height:300,
+    autoEl: {tag: 'img', src: "../images/DefaultArtist.png"},
+    updateSrc :function(r){
+        if (r.data.strThumb == "")  {
+            this.el.dom.src = "../images/DefaultArtist.png"
+        }
+        else {
+            var thumb =  r.data.strThumb.replace(/<\/thumb>/g, "");
+            thumb = thumb.replace(/<thumb>/g, "");
+            var thumbCrc = FindCRC("actor"+r.data.strActor);
+            //console.log(r.data.strActor,' - ',thumbCrc);
+            this.el.dom.src = "../../vfs/special://masterprofile/Thumbnails/Video/"+thumbCrc.substring(0,1)+"/"+thumbCrc+".tbn";
+        }
+    }
 });
 
 var ActorcolModel = new Ext.grid.ColumnModel([
-		{header: "#", dataIndex: 'idActor', hidden: true},
-		{header: "Actor", dataIndex: 'strActor', width: 200}
+        {header: "#", dataIndex: 'idActor', hidden: true},
+        {header: "Actor", dataIndex: 'strActor', width: 200}
     ]);
 
 var ActorRecord = Ext.data.Record.create([
@@ -182,87 +181,87 @@ var ActorRecord = Ext.data.Record.create([
 ]);
 
 var ActorStore = new Ext.data.Store({
-	sortInfo: {field: 'strActor', direction: "ASC"},
-	reader: new Ext.data.JsonXBMCReader({
-		root:'data'	       
+    sortInfo: {field: 'strActor', direction: "ASC"},
+    reader: new Ext.data.JsonXBMCReader({
+        root:'data'
        }, ActorRecord),
-	listeners: {
+    listeners: {
         beforeload: function(){ setXBMCResponseFormat() }
     },
-	url: '/xbmcCmds/xbmcHttp?command=queryvideodatabase(select idActor, strActor, strThumb FROM actors)' 
+    url: '/xbmcCmds/xbmcHttp?command=queryvideodatabase(select idActor, strActor, strThumb FROM actors)'
 });
 
 ActorGrid = new Ext.grid.GridPanel({
-	cm: ActorcolModel,
-	id: 'actorGrid',
-	title: 'Manage Actors',
-	enableDragDrop: false,
-	stripeRows: true,
-	viewconfig: {forceFit: true},
-	selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
-	region: 'center',
-	height: 620,
-	loadMask:true,
-	//split: true,
-	tbar: [{
-		text: 'Add',
-		iconCls: 'silk-add',
-		handler: onAddActor
-	}, '-', {
-		text: 'Delete',
-		iconCls: 'silk-delete',
-		handler: onDeleteActor
-	}, '-'],
-	store: ActorStore
-}); 
+    cm: ActorcolModel,
+    id: 'actorGrid',
+    title: 'Manage Actors',
+    enableDragDrop: false,
+    stripeRows: true,
+    viewconfig: {forceFit: true},
+    selModel: new Ext.grid.RowSelectionModel({singleSelect: true}),
+    region: 'center',
+    height: 620,
+    loadMask:true,
+    //split: true,
+    tbar: [{
+        text: 'Add',
+        iconCls: 'silk-add',
+        handler: onAddActor
+    }, '-', {
+        text: 'Delete',
+        iconCls: 'silk-delete',
+        handler: onDeleteActor
+    }, '-'],
+    store: ActorStore
+});
 
 // ----------------- General functions --------------------------
 
 function onAddActor(btn, ev) {
-	Ext.Msg.alert('This feature is not implemented');
+    Ext.Msg.alert('This feature is not implemented');
 }
 
 
 function onDeleteActor() {
     var rec = ActorGrid.getSelectionModel().getSelected();
     if (( ActorMovieStore.getCount() !=0  || ActorTvshowStore.getCount() != 0) || ActorEpisodeStore.getCount() !=0 ) {
-		Ext.Msg.alert('Error', 'this Actor is still in use');
-	}
-	else {
-	ActorGrid.store.remove(rec);
-	removeXbmcActor(rec)
-	}
+        Ext.Msg.alert('Error', 'this Actor is still in use');
+    }
+    else {
+    ActorGrid.store.remove(rec);
+    removeXbmcActor(rec)
+    }
 }
 
 function onAddActorEpisode(btn, ev) {
-	Ext.Msg.alert('This feature is not implemented');
+    Ext.Msg.alert('This feature is not implemented');
 }
 
 function onDeleteActorEpisode() {
     var rec = ActorEpisodeGrid.getSelectionModel().getSelected();
-	
-	ActorEpisodeGrid.store.remove(rec);
-	removeXbmcActorEpisode(rec)
+
+    ActorEpisodeGrid.store.remove(rec);
+    removeXbmcActorEpisode(rec)
 }
 
 function onAddActorTVShow(btn, ev) {
-	Ext.Msg.alert('This feature is not implemented');
+    Ext.Msg.alert('This feature is not implemented');
 }
 
 function onDeleteActorTVShow() {
     var rec = ActorTvshowGrid.getSelectionModel().getSelected();
-	
-	ActorTvshowGrid.store.remove(rec);
-	removeXbmcActorTVShow(rec)
+
+    ActorTvshowGrid.store.remove(rec);
+    removeXbmcActorTVShow(rec)
 }
 
 function onAddActorMovie(btn, ev) {
-	Ext.Msg.alert('This feature is not implemented');
+    Ext.Msg.alert('This feature is not implemented');
 }
 
 function onDeleteActorMovie() {
     var rec = ActorMovieGrid.getSelectionModel().getSelected();
-	
-	ActorMovieGrid.store.remove(rec);
-	removeXbmcActorMovie(rec)
+
+    ActorMovieGrid.store.remove(rec);
+    removeXbmcActorMovie(rec)
 }

commit 4dc142e60cb6f75f1317337c9a9861ca2e1f35f6

Fix the TV section that i broke with 683684c7
This fixes issue #57

tvshows/include.js
@@ -54,13 +54,13 @@ function updateXBMCAll() {
             if (v == 1) {
                 myText = 'Checking changes...';
                 if (EpisodedetailPanel.getForm().isDirty()) {
-                    updateXBMCTables(EpisodedetailPanel.getForm(), 'episode', 'episode', EpisodeGrid.getSelectionModel().getSelected().data.episodeid);
+                    updateXBMCTables(EpisodedetailPanel.getForm(), 'episode', EpisodeGrid.getSelectionModel().getSelected().data.episodeid);
                     myText = 'updating Episode info';
                 }
             }
                if (v == 10) {
                 if (TVShowdetailPanel.getForm().isDirty()) {
-                    updateXBMCTables(TVShowdetailPanel.getForm(), 'tvshow', 'tvshow', TvShowGrid.getSelectionModel().getSelected().data.tvshowid);
+                    updateXBMCTables(TVShowdetailPanel.getForm(), 'tvshow', TvShowGrid.getSelectionModel().getSelected().data.tvshowid);
                     myText = 'updating TV Show info';
                         //need commit here
                 }