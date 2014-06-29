037bbb1981a6e32a215f8946463e398c6222e9ce
Andrew Fyfe <andrew@neptune-one.net>
2014-06-29 17:40:35 +0100

    Remove unused FindCRC function.

--- global.js
+++ global.js
@@ -77,27 +77,6 @@ function parseXBMCXml(xmlString) {
     return tempTable;
 }
 
-// Credit to Fiasco from the xbmc forum
-function FindCRC(data) {
-  var CRC=0xffffffff;
-  data = data.toLowerCase();
-  for (var j=0; j<data.length; j++) {
-    var c = data.charCodeAt(j);
-    CRC ^= c << 24;
-    for(var i = 0; i<8; i++) {
-        if(CRC.unsign(8) & 0x80000000) {
-          CRC = (CRC << 1) ^ 0x04C11DB7;
-        } else{
-          CRC <<= 1;
-        }
-     }
-  }
-  if ( CRC < 0 ) CRC = CRC>>>0;
-  var temp = CRC.toString(16);
-  if (temp.length < 8) { return "0"+temp}
-    else return CRC.toString(16);
-};
-
 function copyXBMCVideoThumb(thumb, r, element, type) {
 
     var src = 'special://profile/Thumbnails/Video/'+thumb.substring(0,1)+'/'+thumb+'.tbn';
