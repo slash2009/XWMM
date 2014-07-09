0d3ee2d3554aea354e004f9a8662a4e2f7c0f57e
Andrew Fyfe <andrew@neptune-one.net>
2014-07-09 18:35:27 +0100

    Add header icon that was missed in a7f7ee5

--- resources/js/app/tvshows/tvshow.js
+++ resources/js/app/tvshows/tvshow.js
@@ -415,8 +415,8 @@ var episodeGrid = new Ext.grid.GridPanel({
     cm: new Ext.grid.ColumnModel([
         { header: '#', dataIndex: 'episode', width: 30 },
         { header: 'Title', dataIndex: 'title', id: 'title' },
-        { header: '&#160;', dataIndex: 'playcount', width: 30, tooltip: 'Watched',
-          renderer: checkWatched }
+        { header: '<img src="../resources/images/icons/checked.png" width="16" height="16" alt="Watched">',
+          dataIndex: 'playcount', width: 30, tooltip: 'Watched', renderer: checkWatched }
     ]),
     autoExpandColumn: 'title',
     enableColumnResize: false,
