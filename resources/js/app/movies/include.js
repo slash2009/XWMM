52f5db74106cf47cf1228e51b02398f41fa45605
Andrew Fyfe <andrew@neptune-one.net>
2014-07-09 22:45:46 +0100

    updateXBMCSet no longer required.
    It's now handled with everything else in updateXBMCTables.

--- resources/js/app/movies/include.js
+++ resources/js/app/movies/include.js
@@ -44,40 +44,6 @@ function setUnwatched() {
     }
 }
 
-/**
- * Save set changes back to XBMC.
- * @param {Ext.form.Field} setField The set field.
- */
-function updateXBMCSet(setField) {
-    var newValue = setField.getValue();
-
-    if (setField.originalValue === newValue) {
-        // No change, don't save the value.
-        return;
-    }
-
-    var movieGrid = Ext.getCmp('Moviegrid');
-    var selectedMovie = movieGrid.getSelectionModel().getSelected();
-
-    var rpcCmd = {
-        jsonrpc: '2.0',
-        method: 'VideoLibrary.SetMovieDetails',
-        params: {
-            movieid: selectedMovie.data.movieid,
-            set: newValue
-        },
-        id: 'XWMM'
-    };
-
-    //console.debug('XWMM::updateXBMCSet rpcCmd: ' + rpcCmd);
-    xbmcJsonRPC(rpcCmd);
-
-    setField.IsDirty = false;
-    setField.originalValue = newValue;
-    selectedMovie.data.set = newValue;
-    movieGrid.getView().refresh();
-}
-
 function updateXBMCAll() {
     Ext.MessageBox.show({
         title: 'Please wait',
@@ -107,12 +73,6 @@ function updateXBMCAll() {
                         updateXBMCTables(form, 'movie',
                             Ext.getCmp('Moviegrid').getSelectionModel().getSelected().data.movieid);
                         mesg = 'updating movie info';
-                    }
-
-                    form = Ext.getCmp('moviesetcombo');
-                    if (form.isDirty()) {
-                        updateXBMCSet(form);
-                        mesg = 'updating Sets';
                     }
 
                     form = Ext.getCmp('filedetailPanel').getForm();
